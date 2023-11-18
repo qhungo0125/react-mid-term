6e5379798a1c057ace3e8a7043fbf268e944370e
Hungy <hungngojan25@gmail.com>
2023-11-18 13:14:06 +0700

    update welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,6 @@ function Home() {
   return (
     <>
       <h1>Welcome!</h1>
-      <p>
-        Check out the <Link to="/blog">blog</Link> or the{' '}
-        <Link to="users">users</Link> section
-      </p>
     </>
   );
 }
